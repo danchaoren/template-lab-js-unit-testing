ff0836f230df8a0cc816343dd31b094c61b737dd
HONGTAO <danchaoren@Crixus-Laptop.localdomain>
2019-08-10 15:59:53 +0100

    second commit

--- introduction.test.js
+++ introduction.test.js
@@ -3,5 +3,8 @@ const introduction = require("./introduction.js")
 test("introduce hannah", function(){
 	expect(introduction("hannah")).toBe("hello my name is hannah");
 
+})
+test('introduce nobody',function(){
+	expect(introduction("")).toBe("hello my name is");
 }
 );
